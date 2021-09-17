bc8a70b7ff4d3da8cb219cf637b90b41b0db0ff5
Wyatt <wy6428@163.com>
2021-09-17 16:59:18 +0800

    异常处理

---
+++ 1.ErrHandle/errHandle.go
@@ -0,0 +1,25 @@
+package __ErrHandle
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+)
+
+func queryDataFromDB(db sql.DB) ( res []string , err error)  {
+
+	rows, err := db.Query("select id, name from users where id = ?", 1)
+	if errors.Is(err, sql.ErrNoRows) {
+		log.Println("result is empty")
+		return res, err
+	} else {
+		log.Fatal(err)
+		return res, err
+	}
+
+	for rows.Next() {
+		//todo
+
+	}
+	return
+}
